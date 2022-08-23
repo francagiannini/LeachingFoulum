e7a946989efcfeb5ca56578bfad251974453a313
francagiannini <francagianninikurina@gmail.com>
2022-08-23 18:28:48 +0200

    23_08

--- our_func.R
+++ our_func.R
@@ -1,13 +1,8 @@
-# library(tidyverse)
+library(tidyverse)
+library(readxl)
 
-library(dplyr)
-
-# dfN <- read.table(
-#   "C:/Users/Pablo/Downloads/Telegram Desktop/Copy.of.beh.txt",
-#   header = T,
-#   dec=".", sep = "\t",
-#   na.strings = ".") |> 
-#   mutate(date = lubridate::dmy(date))
+beh <- read_excel("beh.xlsx")
+View(beh)
 
 dfN <- beh
 
@@ -26,9 +21,8 @@ for(i in 2:length(dfN_spl)-1){
 }
 
 ci_func <- function(i, p = period_list, id) {
-  #
-  
-  # id <- as.numeric(rownames(i))
+  #browser()
+  #id <- as.numeric(rownames(i))
   
   cat("Mi ID es", id, "\n")
   # cat(rawToChar(as.raw(c(0x4c, 0x61, 0x20, 0x51, 0x75, 0x65, 0x72, 0x65, 0x6d, 
@@ -41,18 +35,22 @@ ci_func <- function(i, p = period_list, id) {
   D_a <- p[1, "drain"]
   D_b <- p[nrow(p), "drain"]
   
-  j <- 2
+  j <- id
   B <- nrow(p)
   
   # if (D_b > 0) {
   
   D_i <- p[id, "drain"]
   
-  f_Di <- (D_a / 2 + sum(p[j:(id - 1), "drain"]) + D_i / 2) /
-    (D_a / 2 + sum(p[j:(B - 1), "drain"]) + D_b / 2)
+  f_Di <- ((D_a / 2) + sum(p[2:(id - 1), "drain"]) + (D_i / 2)) /
+    ((D_a / 2) + sum(p[2:(B - 1), "drain"]) + (D_b / 2))
+  
+  f_Di <- ifelse(is.na(f_Di),0,f_Di)
   
   C_i = C_a + (C_b - C_a) * f_Di
-  C_i
+  
+  data.frame(id, C_i)
+  
   # } else {
   # Pensar si esta bien
   # ((C_b - C_a) / B) * 
@@ -69,7 +67,7 @@ ci_func_df <- function(period_list_df) {
 }
 
 
-a <- lapply(period_list, ci_func_df)
+lapply(period_list, ci_func_df)
 
 
 period_list[[2]] |> mutate(
