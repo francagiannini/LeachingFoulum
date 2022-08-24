35b24a4a67217b4a95e50ef56bdabe19cef3eec4
francagiannini <francagianninikurina@gmail.com>
2022-08-24 18:45:15 +0200

    siiiiiiiiiiiiiiiiii

--- our_func.R
+++ our_func.R
@@ -1,33 +1,42 @@
 library(tidyverse)
 library(readxl)
+library(lubridate)
 
-beh <- read_excel("beh.xlsx")
-View(beh)
+dfN <- read_excel("data/Flakke_DrainAPSIM_2016_17.xlsx") |> 
+  unique() |> 
+  mutate(idx=seq(1,n(),1))
 
-dfN <- beh
 
-breaks_date <- dfN$date[!is.na(dfN$conc)]
-
-dfN_spl <- dfN |> mutate(period=cut(date, dfN$date[!is.na(dfN$conc)])) |> 
+dfN_spl <- dfN |> 
+  mutate(
+    drain=if_else(drain<0,0,drain),
+    period=
+     cut(as.numeric(idx), dfN$idx[!is.na(dfN$conc)],
+         labels=FALSE,
+         include.lowest = TRUE,
+         right=FALSE)
+      ) |>
   group_split(period) 
 
 
 period_list <- list()
 
-for(i in 2:length(dfN_spl)-1){
-  p=as.data.frame(dfN_spl[[i]])
-  p1=as.data.frame(dfN_spl[[i+1]][1,])
-  period_list[[i]] <- rbind(p, p1) |> mutate(index = row_number())
+for(i in 1:length(dfN_spl)){
+  
+  p<-as.data.frame(dfN_spl[[i]])
+  
+  pfin<- as.data.frame(
+    dfN_spl[[ifelse(unique(p$period)==length(dfN_spl),i,i+1)]][ifelse(unique(p$period)==length(dfN_spl),nrow(p),1),])
+  
+  period_list[[i]] <- rbind(p, pfin) |>
+    mutate(index = row_number()) |> unique()
 }
 
 ci_func <- function(i, p = period_list, id) {
   #browser()
-  #id <- as.numeric(rownames(i))
   
-  cat("Mi ID es", id, "\n")
-  # cat(rawToChar(as.raw(c(0x4c, 0x61, 0x20, 0x51, 0x75, 0x65, 0x72, 0x65, 0x6d, 
-  #                        0x6f, 0x73, 0x20, 0x61, 0x20, 0x46, 0x72, 0x61, 0x6e, 0x63, 0x61
-  # ))), "\n")
+  cat("The ID is", id, "\n")
+  cat("The period is", unique(p$period), "\n")
   
   C_a <- p[1, "conc"]
   C_b <- p[nrow(p), "conc"]
@@ -35,10 +44,7 @@ ci_func <- function(i, p = period_list, id) {
   D_a <- p[1, "drain"]
   D_b <- p[nrow(p), "drain"]
   
-  j <- id
   B <- nrow(p)
-  
-  # if (D_b > 0) {
   
   D_i <- p[id, "drain"]
   
@@ -49,29 +55,46 @@ ci_func <- function(i, p = period_list, id) {
   
   C_i = C_a + (C_b - C_a) * f_Di
   
-  data.frame(id, C_i)
+  res = tibble('beh'=p[id,'beh'],
+               'idx'=p[id,'idx'],
+               'C_i'=ifelse(is.na(p[id,'conc']), C_i,p[id,'conc'])
+               )
   
-  # } else {
-  # Pensar si esta bien
-  # ((C_b - C_a) / B) * 
-  # }
+  return(res)
   
 }
 
-# apply(p[3:nrow(period_list[[3]])-1], 1, ci_func)
 ci_func_df <- function(period_list_df) {
-  sapply(2:(nrow(period_list_df) - 1), function(x, p) {
-    ci_func(p[x], p, id = x)
+  
+  lapply(1:(nrow(period_list_df)), function(x, p) {
     
-  }, p = period_list_df)
+    ci_func(p[x], p, id = x)
+  
+    }, p =  as.data.frame(period_list_df)
+    )
+  
 }
 
+interpol <- do.call(bind_rows, lapply(period_list, ci_func_df)) |> 
+  distinct(idx,.keep_all = TRUE)
 
-lapply(period_list, ci_func_df)
+dfN$nwecoc <- interpol$C_i
+
+ggplot(dfN, 
+       aes(x= date, y=nwecoc, color=as.factor(beh)))+
+  geom_line()+
+  geom_point()+
+  theme_bw()
 
 
-period_list[[2]] |> mutate(
-  ci=ifelse(
-    is.na(conc),
-    ci_func(),#aaca
-    conc))
+# long_df <- long_df |>  
+#   group_by(beh, interpolation_method) |>  
+#   arrange(date) |>
+#   mutate(integrated_outflux= cumsum(drain*interp_conc)) 
+# 
+# ggplot(filter(long_df, beh==73), 
+#        aes(x=date, y=interp_conc*drain, 
+#            color=interpolation_method))+
+#   facet_wrap(~beh)+
+#   geom_line()+
+#   geom_smooth()
