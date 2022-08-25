b83a0686773c178b02860162b736672a815c9323
francagiannini <francagianninikurina@gmail.com>
2022-08-25 18:33:02 +0200

    some explore

--- some_explore.R
+++ some_explore.R
@@ -31,12 +31,44 @@ dfN |> group_by(beh) |>
   ggplot(aes(y=Concentration,x=cumdrain , 
              col=as.factor(interpol)#, fill=as.factor(interpol
              ))+
-  geom_point(aes(col=as.factor(beh)))+
+  geom_point(aes(col=as.factor(beh))) +
   geom_smooth(aes(col=as.factor(beh))) +
   facet_wrap(~beh)+
   theme_bw()
 
+library(lme4)
 
-dfN |> group_by(beh) |> 
-  mutate(cumdrain=cumsum(drain)) |>
-  ggplot(aes())
+nlm_gau=nlme(conc~je+alfa/(1+exp(-(cumdrain-beta)/gamma))
+                             ,data=dfN
+                             ,fixed=list(je~1
+                                         ,alfa~1
+                                         ,beta~1
+                                         ,gamma~1)
+                             ,random=list(je~1
+                                          ,beta~1
+                                          #,gamma~1
+                                          )
+                             ,groups=~beh
+                             ,start=c(0,47,120,47)
+                             ,na.action=na.omit
+                             ,method="REML")
+
+plot(nlm_gau)
+
+summary(nlm_gau)
+
+dfN$concgau <- predict(nlm_gau,dfN)
+
+dfN |> 
+  pivot_longer(cols=c(concgau,nweconc,conc), 
+               names_to = "interpol", values_to = "Concentration") |> 
+  # group_by(beh) |> 
+  # filter(row_number(beh) > 5) |>
+  ggplot(aes(y=Concentration,x=cumdrain , 
+             col=as.factor(interpol)#, fill=as.factor(interpol
+  ))+
+  geom_point(aes(col=as.factor(interpol))) +
+  #geom_line()+
+  #geom_smooth(aes(col=as.factor(beh))) +
+  facet_wrap(~beh)+
+  theme_bw()
